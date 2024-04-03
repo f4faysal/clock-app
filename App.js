26185b0e7119dc261b5610f3544ab5784b38df0a
f4faysal <f4faysals@gmail.com>
2024-04-03 23:50:46 +0600

    add roe View component

--- App.js
+++ App.js
@@ -13,6 +13,46 @@ import {
   Text,
   View,
 } from "react-native";
+
+const RowView = ({ label, value }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 8,
+      }}
+    >
+      <View>
+        <Text
+          style={{
+            fontFamily: "Inter-Regular",
+            color: "#303030",
+            fontSize: 10,
+            letterSpacing: 2,
+            textTransform: "uppercase",
+          }}
+        >
+          {label}
+        </Text>
+      </View>
+
+      <View>
+        <Text
+          style={{
+            fontFamily: "Inter-Bold",
+            fontSize: 20,
+            color: "#303030",
+          }}
+        >
+          {value}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
 export default function App() {
   let [fontsLoaded] = useFonts({
     "Inter-regular": Inter_400Regular,
